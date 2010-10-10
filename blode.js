824d51a7a5d023eaab79b0965f70ad7e452e106b
Ben LeMasurier <ben.lemasurier@gmail.com>
2010-10-09 22:43:13 -0600

    basic socket event broadcat server

--- blode.js
+++ blode.js
@@ -12,23 +12,66 @@
  * 8: none
  *
  */
+
+Array.prototype.remove = function(e) {
+  for (var i = 0; i < this.length; i++) {
+    if (e == this[i]) { return this.splice(i, 1); }
+  }
+};
+
 function Config() {
-    this.listen_ip = "127.0.0.1";
-    this.listen_port = 8000;
+    this.log_ip = "127.0.0.1";
+    this.log_port = 8000;
+    this.broadcast_ip = "127.0.0.1";
+    this.broadcast_port = 8001;
     this.debug = true;
 }
 
-var config = new Config;
-    sys  = require("sys"),
-    http = require("http"),
-    url  = require("url");
+function Client(stream) {
+    this.stream = stream;
+}
 
+var config = new Config;
+    net     = require("net");
+    sys     = require("sys"),
+    http    = require("http"),
+    url     = require("url");
+    event   = require("events")
+    emitter = new event.EventEmitter;
+
+// Broadcat log events
+var clients = [];
+var server  = net.createServer(function(stream) {
+    stream.setEncoding('utf8');
+    stream.on("connect", function() {
+        var client = new Client(stream);
+        clients.push(client);
+
+        stream.on('end', function() {
+            clients.remove(client);
+            client.stream.end();
+        });
+    });
+
+    emitter.on("log", function(severity, message) {
+        clients.forEach(function(client) {
+            client.stream.write("{ severity: " + severity 
+                              + ", message: " + message + " }\r\n");
+        });
+    });
+});
+server.listen(config.broadcast_port, config.broadcast_ip);
+
+// Listen to log events
 http.createServer(function(request, response) {
     var parameters = url.parse(request.url, true).query;
 
     response.writeHead(200, { 
         "Content-Type": "text/html",
     });
+
+    // emit message event
+    emitter.emit("log", parameters.severity, parameters.message);
 
     if(config.debug) {
         console.log((new Date()) + 
@@ -38,6 +81,6 @@ http.createServer(function(request, response) {
     }
 
     response.end();
-}).listen(config.listen_port, config.listen_ip);
+}).listen(config.log_port, config.log_ip);
 
-sys.puts("Server start at http://" + config.listen_ip + ":" + config.listen_port);
+sys.puts("Server start at http://" + config.log_ip + ":" + config.log_port);
---
+++ test/example_netcat_client.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+nc localhost 8001
