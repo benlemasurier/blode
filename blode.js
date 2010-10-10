3963d9114fc46bedd5d5eddb6da3b4200420f237
Ben LeMasurier <ben.lemasurier@gmail.com>
2010-10-09 21:53:48 -0600

    testing

--- blode.js
+++ blode.js
@@ -1,12 +1,17 @@
-var sys  = require("sys"),
+function Config() {
+    var listen_ip = "127.0.0.1",
+        listen_port = 8000,
+        debug = true;
+}
+
+var config = new Config(),
+    sys  = require("sys"),
     http = require("http"),
     url  = require("url");
 
-// to be in some sort of config file
-var listen_ip = "127.0.0.1";
-var listen_port = 8000;
-var debug = true;
-
+var blah = new Config();
+console.log(blah.listen_ip);
+/*
 http.createServer(function(request, response) {
     var parameters = url.parse(request.url, true).query;
 
@@ -14,7 +19,7 @@ http.createServer(function(request, response) {
         "Content-Type": "text/html",
     });
 
-    if(debug) {
+    if(config.debug) {
         console.log((new Date()) + 
             ": received request. " +
             " { message: " + parameters.message + 
@@ -22,6 +27,7 @@ http.createServer(function(request, response) {
     }
 
     response.end();
-}).listen(listen_port);
+}).listen(config.listen_port, config.listen_ip);
 
-sys.puts("Server start at http://" + listen_ip + ":" + listen_port);
+sys.puts("Server start at http://" + config.listen_ip + ":" + config.listen_port);
+*/
