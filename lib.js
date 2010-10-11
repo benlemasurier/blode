3eccb35913096c40c8dffc6d0c82f4b3c32dc767
Ben LeMasurier <ben.lemasurier@gmail.com>
2010-10-11 10:23:25 -0600

    minor library optimization

--- lib.js
+++ lib.js
@@ -2,7 +2,7 @@
  * blode support library
  */
 Array.prototype.remove = function(e) {
-  for (var i = 0; i < this.length; i++) {
+  for(var i = 0, j = this.length; i < j; i++) {
     if(e == this[i])
       return(this.splice(i, 1)); 
   }
